11f71f46244b6cde2f6b10fbca22b198a798bbce
mitubhungani <bhunganimitu@gmail.com>
2024-05-21 22:55:31 +0530

    8

--- js/showpro.js
+++ js/showpro.js
@@ -1,4 +1,7 @@
 import GetData from "../admin/components/get.js";
+import Footer from "../components/footer.js";
+
+document.getElementById("footer").innerHTML= Footer();
 
 const ui = (data) => {
   document.querySelector(".pro-image").innerHTML = "";
@@ -17,15 +20,66 @@ const ui = (data) => {
     imgs.src = ele.img[0];
     imgs.setAttribute("id","pro-img")
     innerbox1.append(imgs);
+
+    let title= document.createElement('h2')
+    title.innerHTML=ele.title
+    title.setAttribute("id","title")
+
+    let desc = document.createElement('p')
+    desc.innerHTML=ele.desc
+    desc.setAttribute("id","desc")
+
+    let buybtn = document.createElement("button")
+    buybtn.innerHTML="Buy Now"
+    buybtn.setAttribute("id","buybtn")
+
+
+    innerbox2.append(title,desc,buybtn)
     
     box.append(innerbox1,innerbox2)
     document.querySelector(".pro-image").append(box);
   });
 };
 const Get = async () => {
-  let res = await GetData("http://localhost:3000/products");
+  let res = await GetData(`http://localhost:3000/products?id=14e1`);
   
   ui(res);
 };
 
-Get();
+Get()
+
+const ui2=(inf)=>{
+  document.querySelector(".secondpro").innerHTML = "";
+
+  inf.map((ele,i)=>{
+    let main= document.createElement("div")
+    main.setAttribute("id","main")
+
+    let imgdiv= document.createElement("div")
+    imgdiv.setAttribute("id","imgdiv")
+
+    let pro= document.createElement("img")
+    pro.src=ele.img[0]
+    pro.setAttribute("id","pro")
+
+    imgdiv.append(pro)
+
+    let tit= document.createElement("h4")
+    tit.innerHTML=ele.title
+    tit.setAttribute("id","tit")
+
+    let buybtn= document.createElement("button")
+    buybtn.innerHTML="Buy Now"
+    buybtn.setAttribute("id","buybtn2")
+
+    main.append(imgdiv,tit,buybtn)
+    document.querySelector(".secondpro").append(main)
+  })
+}
+
+const Get2 = async () => {
+  let res = await GetData(`http://localhost:3000/products`);
+  
+  ui2(res);
+}
+Get2();
